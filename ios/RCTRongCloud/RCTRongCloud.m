d53c864107314d8cfb8c96a1022d384a78311e35
windwang <windwang@gmail.com>
2016-12-06 17:52:20 +0800

    修改获取到的位置信息图片的内容

--- ios/RCTRongCloud/RCTRongCloud.m
+++ ios/RCTRongCloud/RCTRongCloud.m
@@ -506,7 +506,7 @@ RCT_EXPORT_METHOD(stopPlayVoice)
     }else if([messageContent isKindOfClass:[RCLocationMessage class]]){
         RCLocationMessage *message=(RCLocationMessage*)messageContent;
         dic[@"type"]=@"location";
-        dic[@"imageUrl"]= [NSString stringWithFormat:@"data:image/png;base64,%@", [UIImagePNGRepresentation(message.thumbnailImage) base64EncodedStringWithOptions:0]];;
+        //dic[@"content"]= [NSString stringWithFormat:@"data:image/png;base64,%@", [UIImagePNGRepresentation(message.thumbnailImage) base64EncodedStringWithOptions:0]];;
         dic[@"poi"]=message.locationName;
         dic[@"lat"]=@(message.location.latitude);
         dic[@"lng"]=@(message.location.longitude);
